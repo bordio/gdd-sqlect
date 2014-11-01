026868e96197a4cdf729599f0ba673d049e41d04
knofm <canofedericomartin@gmail.com>
2014-11-01 20:46:52 -0300

    ABM de Hotel -> Alta funcionando y validando.

---
+++ src/FrbaHotel/FrbaHotel/ABM de Hotel/Alta_Hotel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.ABM_de_Hotel
+{
+    public partial class Alta_Hotel : Form
+    {
+        private AltaHotelApplicationModel appModel = new AltaHotelApplicationModel();
+        private DataGridView listaHoteles;
+        public Alta_Hotel(DataGridView lsHoteles)
+        {
+            listaHoteles = lsHoteles;
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSeleccionarFecha_Click(object sender, EventArgs e)
+        {
+            monthCalendar1.Visible = true;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            Fecha_creacion.Clear();
+            Fecha_creacion.AppendText(monthCalendar1.SelectionStart.ToShortDateString());
+            monthCalendar1.Visible = false;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Nombre.ResetText();
+            Email.ResetText();
+            Fecha_creacion.ResetText();
+            Calle.ResetText();
+            Nro_calle.ResetText();
+            Pais.ResetText();
+            Ciudad.ResetText();
+            Cantidad_Estrellas.ResetText();
+            ckAllInclusive.Checked = false;
+            ckAllInclusiveModerado.Checked = false;
+            ckMediaPension.Checked = false;
+            ckPensionCompleta.Checked = false;
+        }
+
+        private void btAlta_Click(object sender, EventArgs e)
+        {
+            StringBuilder errores = new StringBuilder();
+            bool retValue = this.appModel.altaHotel(Nombre, Email, Cantidad_Estrellas, Fecha_creacion,
+                this.ckAllInclusive.Checked, this.ckAllInclusiveModerado.Checked, this.ckMediaPension.Checked,
+                this.ckPensionCompleta.Checked, Pais, Ciudad, Calle, Nro_calle, errores);
+
+            if (retValue)
+            {
+                MessageBox.Show("Alta exitosa", "Alta de Hotel", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.listaHoteles.DataSource = ABM_de_Hotel.MainHotel.cargar_lista().DefaultView;
+                this.listaHoteles.AllowUserToAddRows = false;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(errores.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                errores = null;
+            }
+        }
+    }
+}
---
+++ src/FrbaHotel/FrbaHotel/ABM de Hotel/MainHotel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using FrbaHotel.Commons.Database;
+
+namespace FrbaHotel.ABM_de_Hotel
+{
+    public partial class MainHotel : Form
+    {
+        public MainHotel()
+        {
+            InitializeComponent();
+        }
+
+        private void MainHotel_Load(object sender, EventArgs e)
+        {
+            lstHoteles.DataSource = cargar_lista().DefaultView;
+            lstHoteles.AllowUserToAddRows = false;
+        }
+
+        public static DataTable cargar_lista()
+        {
+            StringBuilder sentence = new StringBuilder().AppendFormat("SELECT nombre 'Nombre', pais 'Pais', ciudad 'Ciudad', calle + ' ' + CONVERT(varchar,nro_calle,10) 'Direccion', cant_estrellas 'Cantidad de estrellas' FROM SQLECT.Hoteles");
+            DataTable tabla = Conexion.Instance.ejecutarQuery(sentence.ToString());
+
+            return tabla;
+        }
+
+        private void cerrar_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void modificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstHoteles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void agregar_Click(object sender, EventArgs e)
+        {
+            Alta_Hotel formAlta = new Alta_Hotel(this.lstHoteles);
+            formAlta.Show();
+        }
+    }
+}
