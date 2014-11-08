683eebbf087a4f5ec41df91d38ca1ae0f11365bd
knofm <canofedericomartin@gmail.com>
2014-11-08 19:01:56 -0300

    Init de ABM de Habitaciones

---
+++ src/FrbaHotel/FrbaHotel/ABM de Habitacion/MainHabitacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.ABM_de_Habitacion
+{
+    public partial class MainHabitacion : Form
+    {
+        public MainHabitacion()
+        {
+            InitializeComponent();
+        }
+
+        private void btVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btAlta_Click(object sender, EventArgs e)
+        {
+            FormHabitacion formAlta = new FormHabitacion(new AltaHabitacionAppModel());
+            formAlta.Show();
+            formAlta.Text = "Alta de habitacion";
+        }
+
+        private void btModificar_Click(object sender, EventArgs e)
+        {
+            FormHabitacion formModificacion = new FormHabitacion(new ModificacionHabitacionAppModel());
+            formModificacion.Show();
+            formModificacion.Text = "Modificacion de habitacion";
+        }
+    }
+}
