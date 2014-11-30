ea3a5ec51f3d1cb2d7c632108567d627924cc482
German Loiberg <german.loiberg@gmail.com>
2014-11-30 16:03:17 -0300

    Registro de consumibles (además: modificación y borrado). Creo la tabla correspondiente en la BD y demás procedures.

---
+++ src/FrbaHotel/FrbaHotel/Registrar Consumible/AppModel_Consumibles.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using FrbaHotel.Commons.Database;
+using System.Windows.Forms;
+using FrbaHotel.Commons.FuncionalidadesVarias;
+
+namespace FrbaHotel.Registrar_Consumible
+{
+    class AppModel_Consumible
+    {
+        private Conexion sqlconexion = Conexion.Instance;
+        Funcionalidades funcionesVarias = new Funcionalidades();
+
+
+        public DataTable cargarConsumibles(string codigoReservaActual)
+    {
+        StringBuilder sentence = new StringBuilder().AppendFormat(string.Format("SELECT DISTINCT h.nro_habitacion,c.descripcion,ceh.cantidad FROM SQLECT.Consumibles_Estadias_Habitaciones ceh JOIN SQLECT.Consumibles c ON (ceh.fk_consumible=c.id_consumible) JOIN SQLECT.Habitaciones h ON (ceh.fk_habitacion=h.id_habitacion) JOIN SQLECT.Estadias e ON (e.id_estadia=ceh.fk_estadia) JOIN SQLECT.Reservas r ON (e.fk_reserva=r.id_reserva) WHERE r.codigo_reserva='{0}' AND ceh.cantidad>0", codigoReservaActual));
+        DataTable tablaConsumibles = Conexion.Instance.ejecutarQuery(sentence.ToString());
+
+        return tablaConsumibles;
+    }
+        
+        
+        public bool verificarHabitacionDeReserva(string codigoReserva, int numeroHabitacion)
+        {
+            Conexion conexion = Conexion.Instance;
+            System.Data.SqlClient.SqlCommand comandoAReserva = new System.Data.SqlClient.SqlCommand();
+            comandoAReserva.CommandType = CommandType.StoredProcedure;
+
+            comandoAReserva.Parameters.Add("@codigoReserva", SqlDbType.VarChar);
+            comandoAReserva.Parameters.Add("@numeroHabitacion", SqlDbType.Int);
+
+            comandoAReserva.Parameters[0].Value = codigoReserva;
+            comandoAReserva.Parameters[1].Value = numeroHabitacion;
+
+            comandoAReserva.CommandText = "SQLECT.verificarHabitacionDeReserva";
+            bool existenciaDeHabitacion = conexion.ejecutarEscalar(comandoAReserva);
+
+            return existenciaDeHabitacion;
+        
+        }
+
+        public void registrarConsumible(string codigoReserva, int numeroHabitacion, string consumible, int cantidadConsumida)
+        {
+            Conexion conexion = Conexion.Instance;
+            System.Data.SqlClient.SqlCommand comandoAReserva = new System.Data.SqlClient.SqlCommand();
+            comandoAReserva.CommandType = CommandType.StoredProcedure;
+
+            comandoAReserva.Parameters.Add("@codigoReserva", SqlDbType.VarChar);
+            comandoAReserva.Parameters.Add("@numeroHabitacion", SqlDbType.Int);
+            comandoAReserva.Parameters.Add("@consumible", SqlDbType.VarChar);
+            comandoAReserva.Parameters.Add("@cantidadConsumida", SqlDbType.Int);
+
+            comandoAReserva.Parameters[0].Value = codigoReserva;
+            comandoAReserva.Parameters[1].Value = numeroHabitacion;
+            comandoAReserva.Parameters[2].Value = consumible;
+            comandoAReserva.Parameters[3].Value = cantidadConsumida;
+
+            comandoAReserva.CommandText = "SQLECT.registrarConsumible";
+            conexion.ejecutarSP(comandoAReserva);
+        
+        }
+
+        public void modificarConsumible(string codigoReserva, int numeroHabitacion, string consumible, int cantidadConsumida)
+        {
+            Conexion conexion = Conexion.Instance;
+            System.Data.SqlClient.SqlCommand comandoAReserva = new System.Data.SqlClient.SqlCommand();
+            comandoAReserva.CommandType = CommandType.StoredProcedure;
+
+            comandoAReserva.Parameters.Add("@codigoReserva", SqlDbType.VarChar);
+            comandoAReserva.Parameters.Add("@numeroHabitacion", SqlDbType.Int);
+            comandoAReserva.Parameters.Add("@consumible", SqlDbType.VarChar);
+            comandoAReserva.Parameters.Add("@cantidadConsumida", SqlDbType.Int);
+
+            comandoAReserva.Parameters[0].Value = codigoReserva;
+            comandoAReserva.Parameters[1].Value = numeroHabitacion;
+            comandoAReserva.Parameters[2].Value = consumible;
+            comandoAReserva.Parameters[3].Value = cantidadConsumida;
+
+            comandoAReserva.CommandText = "SQLECT.modificarConsumible";
+            conexion.ejecutarSP(comandoAReserva);
+        
+        }
+        public void eliminarConsumible(string codigoReserva, int numeroHabitacion, string consumible)
+        {
+            Conexion conexion = Conexion.Instance;
+            System.Data.SqlClient.SqlCommand comandoAReserva = new System.Data.SqlClient.SqlCommand();
+            comandoAReserva.CommandType = CommandType.StoredProcedure;
+
+            comandoAReserva.Parameters.Add("@codigoReserva", SqlDbType.VarChar);
+            comandoAReserva.Parameters.Add("@numeroHabitacion", SqlDbType.Int);
+            comandoAReserva.Parameters.Add("@consumible", SqlDbType.VarChar);
+            
+            comandoAReserva.Parameters[0].Value = codigoReserva;
+            comandoAReserva.Parameters[1].Value = numeroHabitacion;
+            comandoAReserva.Parameters[2].Value = consumible;
+            
+            comandoAReserva.CommandText = "SQLECT.eliminarConsumible";
+            conexion.ejecutarSP(comandoAReserva);
+        
+        }
+    
+
+
+    }
+
+
+}
--- src/FrbaHotel/FrbaHotel/Registrar Consumible/Form1.Designer.cs
+++ src/FrbaHotel/FrbaHotel/Registrar Consumible/Form1.Designer.cs
@@ -29,40 +29,37 @@
         private void InitializeComponent()
         {
             this.tablaDeConsumibles = new System.Windows.Forms.DataGridView();
-            this.botonConfirmar = new System.Windows.Forms.Button();
+            this.botonConfirmarOperacion = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.botonAgregarFila = new System.Windows.Forms.Button();
-            this.numeroHab = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.consumible = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cantidadConsumidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.botonRegistrarConsumible = new System.Windows.Forms.Button();
+            this.botonModificarConsumible = new System.Windows.Forms.Button();
+            this.botonBorrarConsumible = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tablaDeConsumibles)).BeginInit();
             this.SuspendLayout();
             // 
             // tablaDeConsumibles
             // 
+            this.tablaDeConsumibles.AllowUserToAddRows = false;
+            this.tablaDeConsumibles.AllowUserToDeleteRows = false;
             this.tablaDeConsumibles.AllowUserToResizeColumns = false;
             this.tablaDeConsumibles.AllowUserToResizeRows = false;
             this.tablaDeConsumibles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tablaDeConsumibles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.numeroHab,
-            this.consumible,
-            this.cantidadConsumidos});
             this.tablaDeConsumibles.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.tablaDeConsumibles.Enabled = false;
             this.tablaDeConsumibles.Location = new System.Drawing.Point(39, 90);
             this.tablaDeConsumibles.Name = "tablaDeConsumibles";
-            this.tablaDeConsumibles.Size = new System.Drawing.Size(353, 149);
+            this.tablaDeConsumibles.Size = new System.Drawing.Size(353, 143);
             this.tablaDeConsumibles.TabIndex = 0;
-            this.tablaDeConsumibles.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.tablaDeConsumibles_CellEndEdit);
             // 
-            // botonConfirmar
+            // botonConfirmarOperacion
             // 
-            this.botonConfirmar.Location = new System.Drawing.Point(659, 225);
-            this.botonConfirmar.Name = "botonConfirmar";
-            this.botonConfirmar.Size = new System.Drawing.Size(133, 57);
-            this.botonConfirmar.TabIndex = 1;
-            this.botonConfirmar.Text = "Confirmar operación";
-            this.botonConfirmar.UseVisualStyleBackColor = true;
-            this.botonConfirmar.Click += new System.EventHandler(this.botonConfirmar_Click);
+            this.botonConfirmarOperacion.Location = new System.Drawing.Point(767, 268);
+            this.botonConfirmarOperacion.Name = "botonConfirmarOperacion";
+            this.botonConfirmarOperacion.Size = new System.Drawing.Size(149, 57);
+            this.botonConfirmarOperacion.TabIndex = 1;
+            this.botonConfirmarOperacion.Text = "Confirmar operación";
+            this.botonConfirmarOperacion.UseVisualStyleBackColor = true;
+            this.botonConfirmarOperacion.Click += new System.EventHandler(this.botonConfirmar_Click);
             // 
             // label1
             // 
@@ -70,46 +67,50 @@
             this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(36, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(341, 18);
+            this.label1.Size = new System.Drawing.Size(438, 18);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Ingrese los productos que consumió y su cantidad.";
+            this.label1.Text = "Cargue los productos y/o servicios consumidos durante la estadía.";
             // 
-            // botonAgregarFila
+            // botonRegistrarConsumible
             // 
-            this.botonAgregarFila.Location = new System.Drawing.Point(513, 90);
-            this.botonAgregarFila.Name = "botonAgregarFila";
-            this.botonAgregarFila.Size = new System.Drawing.Size(162, 31);
-            this.botonAgregarFila.TabIndex = 3;
-            this.botonAgregarFila.Text = "Registrar consumible";
-            this.botonAgregarFila.UseVisualStyleBackColor = true;
-            this.botonAgregarFila.Click += new System.EventHandler(this.botonAgregarFila_Click);
+            this.botonRegistrarConsumible.Location = new System.Drawing.Point(530, 90);
+            this.botonRegistrarConsumible.Name = "botonRegistrarConsumible";
+            this.botonRegistrarConsumible.Size = new System.Drawing.Size(162, 31);
+            this.botonRegistrarConsumible.TabIndex = 3;
+            this.botonRegistrarConsumible.Text = "Registrar consumible";
+            this.botonRegistrarConsumible.UseVisualStyleBackColor = true;
+            this.botonRegistrarConsumible.Click += new System.EventHandler(this.botonRegistrarConsumible_Click);
             // 
-            // numeroHab
+            // botonModificarConsumible
             // 
-            this.numeroHab.HeaderText = "Numero de Habitacion";
-            this.numeroHab.Name = "numeroHab";
-            this.numeroHab.ReadOnly = true;
+            this.botonModificarConsumible.Location = new System.Drawing.Point(530, 147);
+            this.botonModificarConsumible.Name = "botonModificarConsumible";
+            this.botonModificarConsumible.Size = new System.Drawing.Size(162, 31);
+            this.botonModificarConsumible.TabIndex = 4;
+            this.botonModificarConsumible.Text = "Modificar consumible";
+            this.botonModificarConsumible.UseVisualStyleBackColor = true;
+            this.botonModificarConsumible.Click += new System.EventHandler(this.botonModificarConsumible_Click);
             // 
-            // consumible
+            // botonBorrarConsumible
             // 
-            this.consumible.HeaderText = "Consumible";
-            this.consumible.Name = "consumible";
-            this.consumible.ReadOnly = true;
-            // 
-            // cantidadConsumidos
-            // 
-            this.cantidadConsumidos.HeaderText = "Cantidad";
-            this.cantidadConsumidos.Name = "cantidadConsumidos";
-            this.cantidadConsumidos.ReadOnly = true;
+            this.botonBorrarConsumible.Location = new System.Drawing.Point(530, 202);
+            this.botonBorrarConsumible.Name = "botonBorrarConsumible";
+            this.botonBorrarConsumible.Size = new System.Drawing.Size(162, 31);
+            this.botonBorrarConsumible.TabIndex = 5;
+            this.botonBorrarConsumible.Text = "Borrar consumible";
+            this.botonBorrarConsumible.UseVisualStyleBackColor = true;
+            this.botonBorrarConsumible.Click += new System.EventHandler(this.botonBorrarConsumible_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(993, 354);
-            this.Controls.Add(this.botonAgregarFila);
+            this.Controls.Add(this.botonBorrarConsumible);
+            this.Controls.Add(this.botonModificarConsumible);
+            this.Controls.Add(this.botonRegistrarConsumible);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.botonConfirmar);
+            this.Controls.Add(this.botonConfirmarOperacion);
             this.Controls.Add(this.tablaDeConsumibles);
             this.Name = "Form1";
             this.Text = "Registrar Conumibles";
@@ -123,11 +124,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView tablaDeConsumibles;
-        private System.Windows.Forms.Button botonConfirmar;
+        private System.Windows.Forms.Button botonConfirmarOperacion;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button botonAgregarFila;
-        private System.Windows.Forms.DataGridViewTextBoxColumn numeroHab;
-        private System.Windows.Forms.DataGridViewTextBoxColumn consumible;
-        private System.Windows.Forms.DataGridViewTextBoxColumn cantidadConsumidos;
+        private System.Windows.Forms.Button botonRegistrarConsumible;
+        private System.Windows.Forms.Button botonModificarConsumible;
+        private System.Windows.Forms.Button botonBorrarConsumible;
     }
 }
