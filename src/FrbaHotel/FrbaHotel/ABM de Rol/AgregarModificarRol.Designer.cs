0dea5a5d0e387526f01eeb2868cbcc419e8447da
Mianski <ariel.soromianski@gmail.com>
2014-12-02 00:55:02 -0300

    Puesta al día del ABM Rol. Agregue los SP de nuevo rol y modificacion de rol. Agregué la funcionalidad correspondiente a los mismos. También la activacion y desactivación de un rol

--- src/FrbaHotel/FrbaHotel/ABM de Rol/AgregarModificarRol.Designer.cs
+++ src/FrbaHotel/FrbaHotel/ABM de Rol/AgregarModificarRol.Designer.cs
@@ -33,10 +33,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.txtDescrip = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.chkLstFunc = new System.Windows.Forms.CheckedListBox();
             this.bttnAceptar = new System.Windows.Forms.Button();
             this.bttnCancelar = new System.Windows.Forms.Button();
+            this.chkBxGestRol = new System.Windows.Forms.CheckBox();
+            this.chkBxGestUsr = new System.Windows.Forms.CheckBox();
+            this.chkBxGestCli = new System.Windows.Forms.CheckBox();
+            this.chkBxGestHotel = new System.Windows.Forms.CheckBox();
+            this.chkBxGestHab = new System.Windows.Forms.CheckBox();
+            this.chkBxGestRes = new System.Windows.Forms.CheckBox();
+            this.chkBxCancelRes = new System.Windows.Forms.CheckBox();
+            this.chkBxGestConsu = new System.Windows.Forms.CheckBox();
+            this.chkBxGestEstad = new System.Windows.Forms.CheckBox();
+            this.chkBxGestFactu = new System.Windows.Forms.CheckBox();
+            this.chkBxListados = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -84,54 +93,154 @@
             this.txtDescrip.Size = new System.Drawing.Size(180, 60);
             this.txtDescrip.TabIndex = 1;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(14, 133);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 16);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Funcionalidades";
-            // 
-            // chkLstFunc
-            // 
-            this.chkLstFunc.CheckOnClick = true;
-            this.chkLstFunc.FormattingEnabled = true;
-            this.chkLstFunc.Location = new System.Drawing.Point(127, 133);
-            this.chkLstFunc.Name = "chkLstFunc";
-            this.chkLstFunc.ScrollAlwaysVisible = true;
-            this.chkLstFunc.Size = new System.Drawing.Size(180, 169);
-            this.chkLstFunc.TabIndex = 2;
-            // 
             // bttnAceptar
             // 
-            this.bttnAceptar.Location = new System.Drawing.Point(12, 312);
+            this.bttnAceptar.Location = new System.Drawing.Point(158, 312);
             this.bttnAceptar.Name = "bttnAceptar";
             this.bttnAceptar.Size = new System.Drawing.Size(75, 23);
-            this.bttnAceptar.TabIndex = 3;
+            this.bttnAceptar.TabIndex = 13;
             this.bttnAceptar.Text = "Aceptar";
             this.bttnAceptar.UseVisualStyleBackColor = true;
             this.bttnAceptar.Click += new System.EventHandler(this.bttnAceptar_Click);
             // 
             // bttnCancelar
             // 
-            this.bttnCancelar.Location = new System.Drawing.Point(232, 312);
+            this.bttnCancelar.Location = new System.Drawing.Point(239, 312);
             this.bttnCancelar.Name = "bttnCancelar";
             this.bttnCancelar.Size = new System.Drawing.Size(75, 23);
-            this.bttnCancelar.TabIndex = 4;
+            this.bttnCancelar.TabIndex = 14;
             this.bttnCancelar.Text = "Cancelar";
             this.bttnCancelar.UseVisualStyleBackColor = true;
+            this.bttnCancelar.Click += new System.EventHandler(this.bttnCancelar_Click);
+            // 
+            // chkBxGestRol
+            // 
+            this.chkBxGestRol.AutoSize = true;
+            this.chkBxGestRol.Location = new System.Drawing.Point(20, 171);
+            this.chkBxGestRol.Name = "chkBxGestRol";
+            this.chkBxGestRol.Size = new System.Drawing.Size(96, 17);
+            this.chkBxGestRol.TabIndex = 2;
+            this.chkBxGestRol.Text = "Gestionar roles";
+            this.chkBxGestRol.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestUsr
+            // 
+            this.chkBxGestUsr.AutoSize = true;
+            this.chkBxGestUsr.Location = new System.Drawing.Point(20, 194);
+            this.chkBxGestUsr.Name = "chkBxGestUsr";
+            this.chkBxGestUsr.Size = new System.Drawing.Size(113, 17);
+            this.chkBxGestUsr.TabIndex = 3;
+            this.chkBxGestUsr.Text = "Gestionar usuarios";
+            this.chkBxGestUsr.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestCli
+            // 
+            this.chkBxGestCli.AutoSize = true;
+            this.chkBxGestCli.Location = new System.Drawing.Point(20, 217);
+            this.chkBxGestCli.Name = "chkBxGestCli";
+            this.chkBxGestCli.Size = new System.Drawing.Size(110, 17);
+            this.chkBxGestCli.TabIndex = 4;
+            this.chkBxGestCli.Text = "Gestionar clientes";
+            this.chkBxGestCli.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestHotel
+            // 
+            this.chkBxGestHotel.AutoSize = true;
+            this.chkBxGestHotel.Location = new System.Drawing.Point(20, 240);
+            this.chkBxGestHotel.Name = "chkBxGestHotel";
+            this.chkBxGestHotel.Size = new System.Drawing.Size(108, 17);
+            this.chkBxGestHotel.TabIndex = 5;
+            this.chkBxGestHotel.Text = "Gestionar hoteles";
+            this.chkBxGestHotel.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestHab
+            // 
+            this.chkBxGestHab.AutoSize = true;
+            this.chkBxGestHab.Location = new System.Drawing.Point(20, 263);
+            this.chkBxGestHab.Name = "chkBxGestHab";
+            this.chkBxGestHab.Size = new System.Drawing.Size(134, 17);
+            this.chkBxGestHab.TabIndex = 6;
+            this.chkBxGestHab.Text = "Gestionar habitaciones";
+            this.chkBxGestHab.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestRes
+            // 
+            this.chkBxGestRes.AutoSize = true;
+            this.chkBxGestRes.Location = new System.Drawing.Point(20, 286);
+            this.chkBxGestRes.Name = "chkBxGestRes";
+            this.chkBxGestRes.Size = new System.Drawing.Size(154, 17);
+            this.chkBxGestRes.TabIndex = 7;
+            this.chkBxGestRes.Text = "Generar/modificar reservas";
+            this.chkBxGestRes.UseVisualStyleBackColor = true;
+            // 
+            // chkBxCancelRes
+            // 
+            this.chkBxCancelRes.AutoSize = true;
+            this.chkBxCancelRes.Location = new System.Drawing.Point(158, 171);
+            this.chkBxCancelRes.Name = "chkBxCancelRes";
+            this.chkBxCancelRes.Size = new System.Drawing.Size(111, 17);
+            this.chkBxCancelRes.TabIndex = 8;
+            this.chkBxCancelRes.Text = "Cancelar reservas";
+            this.chkBxCancelRes.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestConsu
+            // 
+            this.chkBxGestConsu.AutoSize = true;
+            this.chkBxGestConsu.Location = new System.Drawing.Point(158, 194);
+            this.chkBxGestConsu.Name = "chkBxGestConsu";
+            this.chkBxGestConsu.Size = new System.Drawing.Size(132, 17);
+            this.chkBxGestConsu.TabIndex = 9;
+            this.chkBxGestConsu.Text = "Gestionar consumibles";
+            this.chkBxGestConsu.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestEstad
+            // 
+            this.chkBxGestEstad.AutoSize = true;
+            this.chkBxGestEstad.Location = new System.Drawing.Point(158, 217);
+            this.chkBxGestEstad.Name = "chkBxGestEstad";
+            this.chkBxGestEstad.Size = new System.Drawing.Size(115, 17);
+            this.chkBxGestEstad.TabIndex = 10;
+            this.chkBxGestEstad.Text = "Gestionar estadías";
+            this.chkBxGestEstad.UseVisualStyleBackColor = true;
+            // 
+            // chkBxGestFactu
+            // 
+            this.chkBxGestFactu.AutoSize = true;
+            this.chkBxGestFactu.Location = new System.Drawing.Point(158, 240);
+            this.chkBxGestFactu.Name = "chkBxGestFactu";
+            this.chkBxGestFactu.Size = new System.Drawing.Size(82, 17);
+            this.chkBxGestFactu.TabIndex = 11;
+            this.chkBxGestFactu.Text = "Facturación";
+            this.chkBxGestFactu.UseVisualStyleBackColor = true;
+            // 
+            // chkBxListados
+            // 
+            this.chkBxListados.AutoSize = true;
+            this.chkBxListados.Location = new System.Drawing.Point(158, 263);
+            this.chkBxListados.Name = "chkBxListados";
+            this.chkBxListados.Size = new System.Drawing.Size(115, 17);
+            this.chkBxListados.TabIndex = 12;
+            this.chkBxListados.Text = "Listado estadístico";
+            this.chkBxListados.UseVisualStyleBackColor = true;
             // 
             // AgregarModificarRol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(326, 347);
+            this.Controls.Add(this.chkBxListados);
+            this.Controls.Add(this.chkBxGestFactu);
+            this.Controls.Add(this.chkBxGestEstad);
+            this.Controls.Add(this.chkBxGestConsu);
+            this.Controls.Add(this.chkBxCancelRes);
+            this.Controls.Add(this.chkBxGestRes);
+            this.Controls.Add(this.chkBxGestHab);
+            this.Controls.Add(this.chkBxGestHotel);
+            this.Controls.Add(this.chkBxGestCli);
+            this.Controls.Add(this.chkBxGestUsr);
+            this.Controls.Add(this.chkBxGestRol);
             this.Controls.Add(this.bttnCancelar);
             this.Controls.Add(this.bttnAceptar);
-            this.Controls.Add(this.chkLstFunc);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtDescrip);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label2);
@@ -139,7 +248,6 @@
             this.Controls.Add(this.lblTitulo);
             this.Name = "AgregarModificarRol";
             this.Text = "AgregarModificarRol";
-           // this.Load += new System.EventHandler(this.AgregarModificarRol_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,10 +260,19 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtDescrip;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.CheckedListBox chkLstFunc;
         private System.Windows.Forms.Button bttnAceptar;
         private System.Windows.Forms.Button bttnCancelar;
+        private System.Windows.Forms.CheckBox chkBxGestRol;
+        private System.Windows.Forms.CheckBox chkBxGestUsr;
+        private System.Windows.Forms.CheckBox chkBxGestCli;
+        private System.Windows.Forms.CheckBox chkBxGestHotel;
+        private System.Windows.Forms.CheckBox chkBxGestHab;
+        private System.Windows.Forms.CheckBox chkBxGestRes;
+        private System.Windows.Forms.CheckBox chkBxCancelRes;
+        private System.Windows.Forms.CheckBox chkBxGestConsu;
+        private System.Windows.Forms.CheckBox chkBxGestEstad;
+        private System.Windows.Forms.CheckBox chkBxGestFactu;
+        private System.Windows.Forms.CheckBox chkBxListados;
 
     }
 }
